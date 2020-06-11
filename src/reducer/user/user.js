mikhailkaryamin/guess-melody-3

7.1 Декомпозирует редьюсер
Разделим большой редьюсер на несколько поменьше, которые будут отвечать только за часть хранилища.

Такие части в программировании называются неймспейсами (namespace).

Затем с помощью метода combineReducers библиотеки Redux соберём эти маленькие части обратно в большой редьюсер.

src/reducer/user/user.js
@@ -1,0 +1,34 @@
+const initialState = {
+  authorizationStatus: AuthorizationStatus.AUTH,
+};
+
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: status,
+    };
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return Object.assign({}, state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+
+export {
+  ActionCreator,
+  ActionType,
+  reducer,
+};

Razzwan/intensive-design-guess-melody

src/reducer/user/user.js
@@ -1,0 +1,34 @@
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => {
+    return {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: status,
+    };
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return Object.assign({}, state, {
+        authorizationStatus: action.payload,
+      });
+  }
+
+  return state;
+};
+
+
+export {
+  ActionCreator,
+  ActionType,
+  reducer,
+};